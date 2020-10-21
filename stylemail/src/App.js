087f9b1e6692fa2b22076be7f38eda035c97eb46
Garrett Erven <gwerven@gmail.com>
2020-10-21 16:27:00 -0400

    Change text

--- stylemail/src/App.js
+++ stylemail/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to StyleMail.
+          Garrett's Change.
         </p>
         <a
           className="App-link"
