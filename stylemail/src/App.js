1e12ada21e9be844eb75ad279b6f2764a3c98bed
alecfarmer <realalecfarmer@gmail.com>
2020-11-19 10:35:33 -0500

    save-push

--- stylemail/src/App.js
+++ stylemail/src/App.js
@@ -1,26 +1,72 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCLs1a1LcEeXEiOAomAG3EodV5Js3LOnAY",
+  authDomain: "stylemail-edf5f.firebaseapp.com",
+  databaseURL: "https://stylemail-edf5f.firebaseio.com",
+  projectId: "stylemail-edf5f",
+  storageBucket: "stylemail-edf5f.appspot.com",
+  messagingSenderId: "506653836700",
+  appId: "1:506653836700:web:b0a2808623db1a3fc0b459",
+  measurementId: "G-F80D8NFME2"
+
+})
+
+const auth = firebase.auth();
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Change
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Test login</h1>
+        <SignOut />
       </header>
+
+      <section>
+        {user ? <Test/> : <SignIn />}
+      </section>
+
     </div>
   );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function Test() {
+  return (
+    <>
+      <p>Congrats, you're in!</p>
+    </>
+  )
 }
 
 export default App;
