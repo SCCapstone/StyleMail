6f9839c2f859aff47f9c594432235512f6c75bdf
khegs99 <47505435+khegs99@users.noreply.github.com>
2021-01-31 17:37:03 -0500

    Delete test.js
    Deleted old behavior test file

--- stylemail/test/specs/test.js
+++
@@ -1,8 +0,0 @@
-const { BrowserRouter } = require("react-router-dom")
-
-describe('webdriver.io page', () => {
-    it('should have the right title', () => {
-        browser.url('https://stylemail.app/')
-        expect(browser).toHaveTitle('StyleMail');
-    })
-})
