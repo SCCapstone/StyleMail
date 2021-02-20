8e0f99c676ec121d0c3632db92b75734435f3691
Garrett Erven <gwerven@gmail.com>
2021-02-20 12:56:28 -0500

    Fixed saved templates js file

--- stylemail/src/components/SavedTemplates.js
+++ stylemail/src/components/SavedTemplates.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-//import React, { useState } from "react"
-=======
 import React from "react"
->>>>>>> ec48302187e91360948196b39282b0e2bfae1d72
 import NavBar from "./NavBar"
 import  "./Dashboard.css"
 
