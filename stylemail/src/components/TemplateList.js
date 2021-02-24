a8b42614f216fccb1ba7445322b37df30a52371d
khegs99 <kmhagan@email.sc.edu>
2021-02-23 19:29:52 -0500

    merged search and edit

--- stylemail/src/components/TemplateList.js
+++ stylemail/src/components/TemplateList.js
@@ -10,6 +10,9 @@ const db = firebase.firestore();
 const TemplateList = () => {
   const [templates, setTemplates] = useState([]);
   const { currentUser } = useAuth()
+  const[searchTerm, setSearchTerm] = useState('')
+ 
+
 
   var userTemps = db.collection('users').doc(currentUser.email).collection('templates');
 
@@ -37,28 +40,55 @@ const TemplateList = () => {
   return (
     <div>
       <div>
-        <h6>Your Custom Templates</h6>
-        <ul>
-          {templates.map(template => (
-            <li key={template.id}>
+        <h6>Templates</h6>
+          <input
+            id="searchbar"
+            type='text'
+            placeholder="Search"   
+            onChange={event =>{setSearchTerm(event.target.value)}}   
+          />
+          {templates.filter((val)=> {
+          if(searchTerm =="") {
+            return val
+          }  
+          else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+            return val
+          }                 
+          }).map((val,key)=>{
+            return (
+            <div className="user" key={key}>
               <div>
-                <div>
-                  <div>{template.title}</div>
-                  <div>{template.html}</div>
+                  <input 
+                    placeholder={val.title}                    
+                    onChange={e=> {
+                      db.collection('users').doc(currentUser.email).collection('templates').doc(val.id).update( {
+                        'title': e.target.value 
+                      })
+                    }  
+                  }
+                    />
+                                                                  
+                  <input 
+                    placeholder={val.html}
+                    onChange={e=> {
+                      db.collection('users').doc(currentUser.email).collection('templates').doc(val.id).update( {
+                        'html': e.target.value 
+                      })
+                    }  
+                  }
+                    />                                       
                 </div>
                 <div
-                  onClick={() => deleteTemplate(template.id)}
+                  onClick={() => deleteTemplate(val.id)}
                   style={{ cursor: 'pointer' }} // add
                 >
-                  <i>delete</i>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+                  <i>Delete</i>
+                  </div>                  
+          </div>
+        );
+      })}    
       </div>
     </div>
   );
 }
-
 export default TemplateList;
