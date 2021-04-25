6b7f7c79c54e534ffda1a5114f296b183a772c06
alecfarmer <realjamesfarmer@gmail.com>
2021-04-25 14:24:15 -0400

    Unit test - p1

--- stylemail/test/test.js
+++ stylemail/test/test.js
@@ -1,30 +1,67 @@
 const assert = require('assert');
-const firebase = require('@firebase/testing');
+const firebase = require('@firebase/rules-unit-testing');
+const fs = require("fs");
 // Variables
 const MY_PROJECT_ID = "stylemail-edf5f"
-const myId = "user_test";
-const theirId = "user_altTest";
-const myAuth = {uid:myId, email: "test@stylemail.com"};
+const myId = "something";
+const theirId = "some";
+const myEmail = "test@stylemail.com"
+const myAuth = {uid:myId};
 
 function getFirestore(auth) {
     return firebase.initializeTestApp({projectId: MY_PROJECT_ID, auth: auth}).firestore();
 }
-// Unit tests
-describe("StyleMail App", () => {
+
+    const rules = fs.readFileSync("../firestore.rules", "utf8");
+    firebase.loadFirestoreRules({ projectId: MY_PROJECT_ID, rules });
+
+// Basic functionality Unit tests
+describe("StyleMail App Basic Functions", () => {
     
     it("A test to test the tests", () => {
         assert.equal(5*2, 10)
     })
-    // Testing if user can edit their own document from the database
-    it("Can write to a user document with the same ID as our user", async() => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection("users").doc(myId);
-        await firebase.assertSucceeds(testDoc.set({foo: "bar"}));
+    it("Can a new user create an account", async(done) => {
+        const db = getFirestore(myAuth)
+        const testDoc = db.collection('users');
+        await firebase.assertSucceeds(testDoc
+            .add({email: 'new@stylemail.com', provider: 'firebase'}).then(done()));
     })
-    // Testing if user can edit a different user's document from the database
-    it("Can't write to a user document with a different ID as our user", async() => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection("users").doc(theirId);
-        await firebase.assertFails(testDoc.set({foo: "bar"}));
+    it("Can a user update their email", async(done) => {
+        const db = getFirestore(myAuth)
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertSucceeds(testDoc
+            .set({email: 'edituser@stylemail.com'}).then(done()));
+    })
+    it("Can a user delete their account", async(done) => {
+        const db = getFirestore(myAuth)
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertSucceeds(testDoc.delete(myId).then(done()));
+    })
+    it("Can a user access their send log", async(done) => {
+        const db = getFirestore(myAuth)
+        const testDoc = db.collection('users').doc(myId).collection('sendlog');
+        await firebase.assertSucceeds(testDoc.get().then(done()));
+    })
+    it("Can a user update their send log", async(done) => {
+        const db = getFirestore(myAuth)
+        const testDoc = db.collection('users').doc(myId).collection('sendlog');
+        await firebase.assertSucceeds(testDoc
+            .add({to: 'friend@gmail.com', subject: 'testing'}).then(done()));
+    })
+    it("Can a user access their templates", async(done) => {
+        const db = getFirestore(myAuth)
+        const testDoc = db.collection('users').doc(myId).collection('templates');
+        await firebase.assertSucceeds(testDoc.get().then(done()));
+    })
+    it("Can a user edit their templates", async(done) => {
+        const db = getFirestore(myAuth)
+        const testDoc = db.collection('users').doc(myId).collection('templates');
+        await firebase.assertSucceeds(testDoc.add({foo: 'bar'}).then(done()));
+    })
+    it("Can a user delete their templates", async(done) => {
+        const db = getFirestore(myAuth)
+        const testDoc = db.collection('users').doc(myId).collection('templates').doc('test-template');
+        await firebase.assertSucceeds(testDoc.delete('test-template').then(done()));
     })
 })
